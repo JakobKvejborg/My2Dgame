57577de7543c863d45ae7c819af84fb5e99e689f
JakobKvejborg <you@example.com>
2023-11-25 19:31:39 +0100

    First commit, start of the game

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,8 +3,9 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     final int originalTitleSize = 16; // 16x16 title pixlels
     final int scale = 3;
@@ -15,5 +16,37 @@ public class GamePanel extends JPanel {
     final int screenWitdth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    Thread gameThread;
 
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWitdth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while (gameThread != null) {
+            System.out.println("The game is running.");
+            // update information such as char positions
+
+            // draw the screen with updated information
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+    }
 }
